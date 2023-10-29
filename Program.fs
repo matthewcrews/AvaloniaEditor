bc163e31bef26a13622e09b7c84079a84c8b4abe
Matthew Crews <matthew@crews.email>
2023-10-29 08:35:36 -0700

    trying

--- Program.fs
+++ Program.fs
@@ -8,6 +8,8 @@ open Avalonia.FuncUI.Hosts
 open Avalonia.FuncUI
 open Avalonia.FuncUI.Elmish
 open Avalonia.Controls.ApplicationLifetimes
+open System
+open System.Reactive.Linq
 
 type MainWindow() as this =
     inherit HostWindow()
@@ -16,9 +18,22 @@ type MainWindow() as this =
         base.Height <- 600.0
         base.Width <- 800.0
 
+        let subscriptions (model: Model) : Sub<Msg> =
+            let editorChangeStream (dispatch: Msg -> unit) =
+                model.EditorChangeStream
+                    .Throttle(TimeSpan.FromSeconds 1)
+                    .Subscribe(fun txt ->
+                        dispatch IncrementCounter
+                    )
+
+            [
+                [ nameof editorChangeStream ], editorChangeStream
+            ]
+
         Elmish.Program.mkProgram Model.init Model.update View.view
         |> Program.withHost this
         |> Program.withConsoleTrace
+        |> Program.withSubscription subscriptions
         |> Program.run
 
 type App() =
--- View.fs
+++ View.fs
@@ -1,48 +1,22 @@
 module AvaloniaEditor.View
 
-open Elmish
-open System
-open Avalonia.FuncUI.Elmish
-open Avalonia.Layout
 open Avalonia.Controls
-open Avalonia.FuncUI
 open Avalonia.FuncUI.DSL
-open Avalonia.FuncUI.Types
-open Avalonia.FuncUI.Elmish.ElmishHook
-open System.Reactive.Subjects
-open System.Reactive.Linq
 open AvaloniaEdit
 
-
-let private subscriptions (model: Model) : Sub<Msg> =
-    let editorChangeStream (dispatch: Msg -> unit) =
-        model.EditorChangeStream
-            .Throttle(TimeSpan.FromSeconds 1)
-            .Subscribe(fun txt ->
-                dispatch IncrementCounter
-            )
-
-    [
-        [ nameof editorChangeStream ], editorChangeStream
-    ]
-
-
 let view (model: Model) (dispatch) =
-    Component (fun ctx ->
-        let model, dispatch = ctx.useElmish(Model.init, Model.update, Program.withSubscription subscriptions)
-        let mutable editor = Unchecked.defaultof<TextEditor>
+    let mutable editor = Unchecked.defaultof<TextEditor>
 
-
-        DockPanel.create [
-            DockPanel.children [
-                Menu.view ()
-                TextEditor.create [
-                    TextEditor.init (fun newEditor ->
-                        editor <- newEditor)
-                    TextEditor.showLineNumbers true
-                    TextEditor.onTextInput (fun _ ->
-                        dispatch (EditorChanged editor)
-                    )
-                ]
+    DockPanel.create [
+        DockPanel.children [
+            Menu.view ()
+            TextEditor.create [
+                TextEditor.init (fun newEditor ->
+                    editor <- newEditor)
+                TextEditor.showLineNumbers true
+                TextEditor.onTextInput (fun _ ->
+                    dispatch (EditorChanged editor)
+                )
             ]
-        ])
+        ]
+    ]
